884e45dafae8bfb7031db3ff3535dd6ecdcb433c
Lucas Maciel <lalucasforte56@gmail.com>
2026-05-06 15:25:17 -0300

    aula2 - exercicio5 adicionado

---
+++ basic/exercises/A2E5.js
@@ -0,0 +1,48 @@
+let prompt = require('prompt-sync')();
+
+function input(message) {
+    return prompt(message);
+}
+
+// let age = parseInt(input("Digite sua idade: "));
+
+// if (age < 18) {
+//     console.log("Você é menor de idade.");
+// }
+// else{
+//     console.log("Você é maior de idade.");
+// }
+
+
+// let n1 = parseInt(input("Digite a primeira nota: "));
+// let n2 = parseInt(input("Digite a segunda nota: "));
+
+// if ((n1+n2)/2 >= 7) {
+//     console.log("Aprovado");
+// }
+// else{
+//     console.log("Reprovado");
+// }
+
+
+// let name = input("Digite seu nome: ");
+// let sex = input("Digite seu sexo (M/F): ");
+
+// if (sex === "M") {
+//     console.log("Bom dia, senhor " + name);
+// }
+// else if (sex === "F") {
+//     console.log("Bom dia, senhora " + name);
+// }
+// else{
+//     console.log("Bom dia, " + name);
+// }
+
+// let number = parseInt(input("Digite um número: "));
+
+// if (number % 2 === 0) {
+//     console.log("O número é par.");
+// }
+// else{
+//     console.log("O número é ímpar.");
+// }
